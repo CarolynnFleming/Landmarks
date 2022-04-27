b5ed7de9573674f4e5b4ec4f549a21a1a106d482
CarolynnFleming <cfcatpink@gmail.com>
2022-04-26 18:21:16 -0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Carolynn Fleming on 4/26/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
